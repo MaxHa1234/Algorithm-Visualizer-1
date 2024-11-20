62332b973e936d04f6bd8f72f260397216b8a21c
MaxHa1234 <xmaximilian998@gmail.com>
2024-11-21 00:31:09 +0100

    infobox first additions

---
+++ algorithm-visualizer/src/app/components/infobox/infobox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infobox',
+  standalone: true,
+  imports: [],
+  templateUrl: './infobox.component.html',
+  styleUrl: './infobox.component.css'
+})
+export class InfoboxComponent {
+
+}
